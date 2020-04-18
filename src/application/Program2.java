bab40ef92c126a5b9433d830e0f3da048bf7ea40
Marcelobbl <produtormarcelo@hotmail.com>
2020-04-18 13:46:56 -0300

    Programa de consulta completo

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -1,5 +1,9 @@
 package application;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 import model.dao.DepartamentoDao;
 import model.dao.FabricaDao;
 import model.entities.Departamento;
@@ -7,6 +11,8 @@ import model.entities.Departamento;
 public class Program2 {
 
 	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
 		
 		DepartamentoDao departamentoDao = FabricaDao.createDepartmentDao();
 		
@@ -16,9 +22,41 @@ public class Program2 {
 		departamentoDao.insert(departamento);
 		System.out.println("Inserido! Novo id = " + departamento.getId());
 		
+		System.out.println("===Test 2: Upadate id:");
+		Departamento dep = departamentoDao.consultaId(1);
+		dep.setNomeDep("Anel");
+		departamentoDao.update(dep);
+		System.out.println(dep);
+		
+		System.out.println("===Test 3: Delete id:");
+		System.out.println("Digite o numero do id para deletar");
+		int numeroId = sc.nextInt();
+		departamentoDao.delete(numeroId);
+		System.out.println("Departamento deletado");
+		
+		System.out.println("===Test 4: consulta id:");
+		dep = departamentoDao.consultaId(2);
+		System.out.println(dep);
+		
+		System.out.println("===Test 5: consulta tudo:");
+		List<Departamento> list = new ArrayList<>();
+		list = departamentoDao.consultaTudo();
+		for(Departamento depList : list) {
+			System.out.println(depList);
+		}
+		
+			
+		
+				
+		
+		
+		
+		
 		
 		
 
+		
+		sc.close();
 	}
 
 }
--- src/model/dao/impl/DepartamentoDaoImplJDBC.java
+++ src/model/dao/impl/DepartamentoDaoImplJDBC.java
@@ -2,8 +2,10 @@ package model.dao.impl;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 import db.DB;
@@ -11,8 +13,8 @@ import db.DbException;
 import model.dao.DepartamentoDao;
 import model.entities.Departamento;
 
-public class DepartamentoDaoImplJDBC implements DepartamentoDao{
-	
+public class DepartamentoDaoImplJDBC implements DepartamentoDao {
+
 	private Connection connection;
 
 	public DepartamentoDaoImplJDBC(Connection connection) {
@@ -23,42 +25,100 @@ public class DepartamentoDaoImplJDBC implements DepartamentoDao{
 	public void insert(Departamento obj) {
 		PreparedStatement st = null;
 		try {
-			st = connection.prepareStatement("INSERT INTO department(Id, Name) VALUES (?,?)", Statement.RETURN_GENERATED_KEYS);
-			st.setInt(1, obj.getId());
-			st.setString(2, obj.getNomeDep());
+			st = connection.prepareStatement("INSERT INTO department " + "(Name) " + "VALUES " + "(?)",
+					Statement.RETURN_GENERATED_KEYS);
 			
-			st.executeUpdate();
-		}
-		catch (SQLException e) {
+			st.setString(1, obj.getNomeDep());
+
+			int linhasGravadas = st.executeUpdate();
+			if (linhasGravadas > 0) {
+				ResultSet rs = st.getGeneratedKeys();
+				if (rs.next()) {
+					int id = rs.getInt(1);
+					obj.setId(id);
+				}
+				DB.closeResultSet(rs);
+
+			}
+		} catch (SQLException e) {
 			throw new DbException(e.getMessage());
-		}
-		finally {
+		} finally {
 			DB.closeStatement(st);
 		}
 	}
 
 	@Override
 	public void update(Departamento obj) {
-		// TODO Auto-generated method stub
-		
+		PreparedStatement st = null;
+		try {
+			st = connection.prepareStatement("UPDATE department " + "SET Name = ? " + "WHERE Id = ?");
+			st.setString(1, obj.getNomeDep());
+			st.setInt(2, obj.getId());
+			st.executeUpdate();
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		} finally {
+			DB.closeStatement(st);
+		}
 	}
 
 	@Override
 	public void delete(Integer id) {
-		// TODO Auto-generated method stub
-		
+		PreparedStatement st = null;
+		try {
+			st = connection.prepareStatement("DELETE FROM department WHERE Id = ?");
+			st.setInt(1, id);
+
+			st.executeUpdate();
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		} finally {
+			DB.closeStatement(st);
+		}
 	}
 
 	@Override
 	public Departamento consultaId(Integer id) {
-		// TODO Auto-generated method stub
-		return null;
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		try {
+			st = connection.prepareStatement("SELECT * FROM department WHERE Id = ?");
+			st.setInt(1, id);
+			rs = st.executeQuery();
+			if (rs.next()) {
+				Departamento dep = new Departamento();
+				dep.setId(rs.getInt("Id"));
+				dep.setNomeDep(rs.getString("Name"));
+				return dep;
+			}
+			return null;
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		} finally {
+			DB.closeStatement(st);
+		}
 	}
 
 	@Override
 	public List<Departamento> consultaTudo() {
-		// TODO Auto-generated method stub
-		return null;
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		try {
+			st = connection.prepareStatement("SELECT * FROM department ORDER BY Name");
+			rs = st.executeQuery();
+			List<Departamento> list = new ArrayList<>();
+			while (rs.next()) {
+				Departamento dep = new Departamento();
+				dep.setId(rs.getInt("Id"));
+				dep.setNomeDep(rs.getString("Name"));
+				list.add(dep);
+			}
+			return list;
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		} finally {
+			DB.closeStatement(st);
+		}
 	}
 
 }
